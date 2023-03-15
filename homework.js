91e49162ff024d8d873144735d50538f01ac4a1e
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-03-15 10:21:40 -0400

    Where is Waldo answered

--- homework.js
+++ homework.js
@@ -1,17 +1,14 @@
 ////////////////////////////////
 // Easy Going
-for(let i = 1; i <= 20; i++)
-{
+for (let i = 1; i <= 20; i++) {
     console.log(i);
 }
 
 
 ////////////////////////////////
 // Get Even
-for(let count = 0; count <= 200; count++)
-{
-    if(count % 2 == 0)
-    {
+for (let count = 0; count <= 200; count++) {
+    if (count % 2 == 0) {
         console.log(count);
     }
 }
@@ -19,19 +16,14 @@ for(let count = 0; count <= 200; count++)
 
 ////////////////////////////////
 // Fizz Buzz
-for(let fizbuz = 1; fizbuz <= 100; fizbuz++)
-{
-    if(fizbuz % 3 == 0 && fizbuz % 5 == 0)
-    {
+for (let fizbuz = 1; fizbuz <= 100; fizbuz++) {
+    if (fizbuz % 3 == 0 && fizbuz % 5 == 0) {
         console.log("FizzBuzz");
-    } else if (fizbuz % 3 == 0 && fizbuz % 5 !== 0)
-    {
+    } else if (fizbuz % 3 == 0 && fizbuz % 5 !== 0) {
         console.log("Fizz")
-    } else if (fizbuz % 5 == 0 && fizbuz % 3 !== 0)
-    {
+    } else if (fizbuz % 5 == 0 && fizbuz % 3 !== 0) {
         console.log("Buzz")
-    } else
-    {
+    } else {
         console.log(fizbuz);
     }
 }
@@ -41,15 +33,13 @@ for(let fizbuz = 1; fizbuz <= 100; fizbuz++)
 // Wild Wild Life
 const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
 const sharky = ["Sharky", "shark", 20, "Left Coast"];
-const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
 const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
 
 //1. Change Plantee's age to reflect her being a year older
-for(let i = 0; i < plantee.length; i++)
-{
-    if(plantee[i] === 5000)
-    {
+for (let i = 0; i < plantee.length; i++) {
+    if (plantee[i] === 5000) {
         plantee[i] += 1;
         console.log(`Plantee is now ${plantee[i]} years old.`);
     }
@@ -68,8 +58,7 @@ wolfy[0] = "Gameboy";
 // Yell at the Ninja Turtles
 let ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
 
-for(element of ninjaTurtles)
-{
+for (element of ninjaTurtles) {
     console.log(element.toUpperCase());
 }
 
@@ -79,10 +68,8 @@ for(element of ninjaTurtles)
 // Methods, Revisited
 const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 
-for(let count = 0; count < favMovies.length; count++)
-{
-    if(favMovies[count] === 'Titanic')
-    {
+for (let count = 0; count < favMovies.length; count++) {
+    if (favMovies[count] === 'Titanic') {
         console.log(favMovies[count]);
     }
 }
@@ -108,11 +95,9 @@ favMovies.shift();
 favMovies.unshift("Joker");
 console.log(favMovies);
 
-for(let fav = 0; fav < favMovies.length; fav++)
-{
-    if(favMovies[fav] === 'Django Unchained')
-    {
-        favMovies.splice(fav,1,'Avatar');
+for (let fav = 0; fav < favMovies.length; fav++) {
+    if (favMovies[fav] === 'Django Unchained') {
+        favMovies.splice(fav, 1, 'Avatar');
     }
 }
 
@@ -130,7 +115,43 @@ console.log(slicedArray);
 
 ////////////////////////////////
 // Where is Waldo
-////////////////////////////////
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+["Lucinda", "Jacc", "Neff", "Snoop"],
+["Petunia", ["Baked Goods", "Waldo"]]];
+
+//1. remove Eggbert
+for(let i = 0; i < whereIsWaldo.length; i++)
+{
+    for(j = 0; j < whereIsWaldo[i].length; j++)
+    {
+        if(whereIsWaldo[j] === 'Eggbert')
+        {
+            whereIsWaldo.splice(j,1);
+        } else if (whereIsWaldo[i] === 'Eggbert')
+        {
+
+            whereIsWaldo.splice(i,1)
+        }
+    }
+}
+
+console.log(whereIsWaldo)
+
+for(let x = 0; x < whereIsWaldo.length; x++)
+{
+    for(let y = 0; y < whereIsWaldo[x].length; y++)
+    {
+        if(whereIsWaldo[x][y] === "Neff") //change if neff is part of a two dimensional array
+        {
+            whereIsWaldo[x][y] = 'No one';
+        } else if (whereIsWaldo[x] === "Neff") //change if neff is not a part of a two dimensional array
+        {
+            whereIsWaldo[x] === 'No one';
+        }
+    }
+}
+
+console.log(whereIsWaldo);
 
 
 ////////////////////////////////
